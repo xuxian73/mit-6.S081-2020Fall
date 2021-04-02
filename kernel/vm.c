535816702477b53f2d13c590d279703e0d13dcc0
xuxian <1532383784@qq.com>
2021-04-02 11:07:58 +0800

    pagetable-1

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void printwalk(pagetable_t pagetable, int level) {
+  char* prefix;
+  if (level == 0) {
+    prefix = "..";
+  } else if (level == 1) {
+    prefix = ".. ..";
+  } else prefix = ".. .. ..";
+  for (int i = 0; i < 512; ++i) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V){
+      printf("%s%d: pte %p pa %p\n", prefix, i, pte, PTE2PA(pte));
+      if (level < 2 && ((pte & (PTE_R|PTE_W|PTE_X)) == 0)) {
+        uint64 child = PTE2PA(pte);
+        printwalk((pagetable_t)child, level + 1);
+      }
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  printwalk(pagetable, 0);
+  return;
+}
